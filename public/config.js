bdc8ba7259b0f3b8d1680162ca830acdf727920a
ThunderboltnGamer <130480785+ThunderboltnGamer@users.noreply.github.com>
2024-05-29 19:55:37 -0500

    Update config.js

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "Place Here",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzlkZWYyZDY5ZWFlNDk4ZjJiOTI4MTgyNDdjM2ViMCIsInN1YiI6IjY2MjdmMGJlNjJmMzM1MDE0YmQ4NTFmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h3KpPvkiaz8uNz1bntAKqsPrxG_4UUWaY3kYME6N6m8",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MjNiMTAxYjRhODMzZjNlMmM4YjAzMjJhMThjZmNmZiIsInN1YiI6IjY2NDdmZmVhMGQ2Y2Q2ZjUwZjJmNTQwNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2zuf5ItNHiEx9RcGngEAW_3QFCE6FtnCAo_emiC4Jos",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
