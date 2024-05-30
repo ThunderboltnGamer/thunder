f7d7bcbb9b8f0235af4365db3ad9c8909f99efc7
ThunderboltnGamer <130480785+ThunderboltnGamer@users.noreply.github.com>
2024-05-29 20:30:40 -0500

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -75,7 +75,8 @@ export function HomePage() {
         ) : (
           <>
             <div className="flex flex-col gap-8">
-              
+              <BookmarksPart onItemsChange={setShowBookmarks} />
+              <WatchingPart onItemsChange={setShowWatching} />
             </div>
           </>
         )}
